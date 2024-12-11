e957fd049d42ab7ac648c3e1f2c07ac28f762013
Afnan Dzaharudin <afnanfdz@proton.me>
2024-12-11 08:01:45 +0000

    moved main

--- main.ts
+++
@@ -1,8 +0,0 @@
-import { Plugin } from 'obsidian';
-import { ConversationsPostProcessor } from 'src/conversationsPostProcessor';
-
-export default class ExamplePlugin extends Plugin {
-  async onload() {
-    this.registerMarkdownPostProcessor(ConversationsPostProcessor);
-  }
-}
